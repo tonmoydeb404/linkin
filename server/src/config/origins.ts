3140c3bddc8697b0618d4728bef05922c241e4da
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-06-12 00:00:31 +0600

    origin edited

--- server/src/config/origins.ts
+++ server/src/config/origins.ts
@@ -1,3 +1,8 @@
-const origins = ["http://localhost:5173", "http://localhost:8080"];
+const origins = [
+  "http://localhost:5173",
+  "http://localhost:4173",
+  "http://localhost:8080",
+  "https://linkin404.netlify.app",
+];
 
 export default origins;
